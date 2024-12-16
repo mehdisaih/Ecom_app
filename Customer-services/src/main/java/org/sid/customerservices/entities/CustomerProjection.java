ff5fb2ddfc97853df9c4f4ec1c4a3c89907f1881
mehdisaih <110493586+mehdisaih@users.noreply.github.com>
2024-12-16 19:23:32 +0100

    add inventory-service

---
+++ Customer-services/src/main/java/org/sid/customerservices/entities/CustomerProjection.java
@@ -0,0 +1,10 @@
+package org.sid.customerservices.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "fullCustomer",types = Customer.class)
+public interface CustomerProjection {
+    public long getId();
+    public String getName();
+
+}
